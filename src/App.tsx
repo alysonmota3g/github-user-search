b976171e2dc4509240fbd95e8ee86e63747e0be7
alysonmota3g <alyson.contact@proton.me>
2022-10-07 10:58:27 -0300

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-zinc-900">
+      Hello World
+    </div>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.tsx", "./src/**/*.tsx", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
